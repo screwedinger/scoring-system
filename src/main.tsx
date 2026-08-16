24deb725ab45e905db97a5b95673b1bd85ba7548
Prithviraj Nalvaya <95399218+screwedinger@users.noreply.github.com>
2026-08-17 03:50:27 +0530

    Wire global undo redo shortcuts

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { installQuizKeyboardShortcuts } from './keyboardShortcuts'
+
+const removeQuizKeyboardShortcuts = installQuizKeyboardShortcuts()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => removeQuizKeyboardShortcuts())
+}
