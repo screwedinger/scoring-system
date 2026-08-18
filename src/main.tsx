4ad527daf6d46df92e62af6bad933efda8a7cc54
Prithviraj Nalvaya <95399218+screwedinger@users.noreply.github.com>
2026-08-18 21:11:15 +0530

    Add end-session archive controls

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Phase1Tools from './Phase1Tools'
+import SessionManager from './SessionManager'
 import { installQuizKeyboardShortcuts } from './keyboardShortcuts'
 
 const removeQuizKeyboardShortcuts = installQuizKeyboardShortcuts()
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Phase1Tools />
+    <SessionManager />
   </StrictMode>,
 )
 
